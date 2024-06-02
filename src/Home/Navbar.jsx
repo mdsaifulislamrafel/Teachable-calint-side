68014d48b5991dc798b27bb5ac2788308d894b45
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-06-02 22:44:44 +0600

    banner added

--- src/Home/Navbar.jsx
+++ src/Home/Navbar.jsx
@@ -1,15 +1,16 @@
 
-
 const Navbar = () => {
   return (
-    <div className="navbar fixed z-40 bg-slate-400">
+    <div className="navbar static z-40 bg-slate-400 ">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">Teach:able</a>
       </div>
       <div className="flex-none gap-2">
-        <div className="form-control">
-          <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-        </div>
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Home</a></li>
+          <li><a>All Classes</a></li>
+        </ul>
+        <button>Sign in</button>
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className='max-w-7xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
