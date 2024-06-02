8ef6fe5a0d58b7b223169537e6b2d17dfe37eb75
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-06-03 00:25:05 +0600

    company logo added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import Company from "./Company";
 import Feature from "./Feature";
 
 const Home = () => {
     return (
         <div className="space-y-5">
             <Banner />
+            <Company />
             <Feature />
         </div>
     );
