d80a2739146fc2c842126a9b7cc371064444644c
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-06-04 18:49:18 +0600

    not use slider code remove

--- src/Home/Feature.jsx
+++ src/Home/Feature.jsx
@@ -142,22 +142,6 @@ const Feature = () => {
                 </SwiperSlide>
             </Swiper>
         </>
-        //     <section className="bg-white dark:bg-gray-900">
-        //     <div className="container px-6 py-10 mx-auto">
-        //         <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
-        //             Explore our <br /> awesome <span className="text-blue-500">Courses</span>
-        //         </h1>
-
-        //         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
-
-
-
-
-
-        //         </div>
-        //     </div>
-        // </section>
-
     );
 };
 
